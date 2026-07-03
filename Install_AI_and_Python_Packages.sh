f3aef1fc66b17900858618bfb30403de937d631e
Varun Chilukuri <130198099+VRUN-dev@users.noreply.github.com>
2026-07-02 19:03:07 -0700

    Update Install_AI_and_Python_Packages.sh

--- Install_AI_and_Python_Packages.sh
+++ Install_AI_and_Python_Packages.sh
@@ -14,3 +14,10 @@ pip install flask opencv-python
 
 # Test YOLO installation
 python3 -c "from ultralytics import YOLO; print('YOLO Installed!')"
+
+#If YOLO says "ModuleNotFoundError"
+source ~/eclipse_lamp/venv/bin/activate
+pip install ultralytics
+
+#If pip upgrades numpy to version 2.x
+pip install numpy==1.26.4 --force-reinstall
